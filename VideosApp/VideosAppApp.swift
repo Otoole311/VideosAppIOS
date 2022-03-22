3b1ee20397756def72cc17245ad6b02bdbe78244
Albert Essilfie <essilfie311@gmail.com>
2022-03-22 14:40:56 +0000

    Initial Commit

---
+++ VideosApp/VideosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideosAppApp.swift
+//  VideosApp
+//
+//  Created by Otoole on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
